4875d8c803519d78cbd1c65cf67e554c119aedc1
Dwika Ilham Zakaria <dwikailham37@gmail.com>
2025-08-13 11:01:58 +0700

    chore(package): init package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.APP_PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello TypeScript + Express!');
