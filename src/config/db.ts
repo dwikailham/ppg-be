28651f9ea514b7ed575c4e008305540c49634f06
Dwika Ilham Zakaria <dwikailham37@gmail.com>
2025-08-13 16:52:49 +0700

    fix(db): timezone local

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,10 @@ import { Sequelize } from 'sequelize';
 const db = new Sequelize('ppg', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
+  timezone: '+07:00', // WIB,
+  dialectOptions: {
+    useUTC: false, // Jangan pakai UTC
+  },
 });
 
 export default db;
