74f5c17d0ae581324214a7092f6c8450261f0a97
Dwika Ilham Zakaria <dwikailham37@gmail.com>
2025-08-13 11:18:57 +0700

    chore(cors): init cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.APP_PORT;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+  })
+);
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello TypeScript + Express!');
