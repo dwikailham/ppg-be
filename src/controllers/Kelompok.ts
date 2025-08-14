8bcf913866c6f258fbf960e58e7bbada2441e94b
Dwika Ilham Zakaria <dwikailham37@gmail.com>
2025-08-14 14:44:19 +0700

    fix(controller/kelompok): add validation submit and update

--- src/controllers/Kelompok.ts
+++ src/controllers/Kelompok.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { KelompokModel, DesaModel } from '../models/index';
 
+type KelompokBody = { name: string; desa_id: number; address: string };
+
 export const getAllKelompok = async (req: Request, res: Response) => {
   try {
     const page = parseInt(req.query.page as string) || 1;
@@ -40,29 +42,63 @@ export const getKelompokById = async (req: Request, res: Response) => {
   }
 };
 
-export const createKelompok = async (req: Request, res: Response) => {
+export const createKelompok = async (
+  req: Request<{}, {}, KelompokBody>,
+  res: Response
+) => {
+  const { name, desa_id, address } = req.body;
+  if (!name || !desa_id) {
+    return res.status(400).json({ message: 'BAD REQUEST' });
+  }
+
+  const existingKelompok = await KelompokModel.findOne({ where: { name } });
+  if (existingKelompok) {
+    return res.status(400).json({ message: 'Nama kelompok sudah digunakan' });
+  }
+
+  const desa = await DesaModel.findByPk(desa_id);
+  if (!desa) {
+    return res.status(400).json({ message: 'Desa tidak ditemukan' });
+  }
+
   try {
-    const kelompok = await KelompokModel.create({
-      desa_id: req.body.desa_id,
-      name: req.body.name,
+    await KelompokModel.create({
+      desa_id,
+      name,
+      address,
     });
-    res.status(201).json(kelompok);
+    res.status(201).json({ message: 'Kelompok berhasil dibuat' });
   } catch (error) {
     res.status(500).json({ message: 'ITNERNAL SERVER ERROR', error });
   }
 };
 
 export const updateKelompok = async (req: Request, res: Response) => {
+  const { name, desa_id, address } = req.body;
+  if (!name || !desa_id) {
+    return res.status(400).json({ message: 'BAD REQUEST' });
+  }
+
+  const existingKelompok = await KelompokModel.findOne({ where: { name } });
+  if (existingKelompok) {
+    return res.status(400).json({ message: 'Nama kelompok sudah digunakan' });
+  }
+
+  const desa = await DesaModel.findByPk(desa_id);
+  if (!desa) {
+    return res.status(400).json({ message: 'Desa tidak ditemukan' });
+  }
   try {
     const kelompok = await KelompokModel.findByPk(req.params.id);
     if (!kelompok)
       return res.status(404).json({ message: 'Kelompok tidak ditemukan' });
 
     await kelompok.update({
-      desa_id: req.body.desa_id,
-      name: req.body.name,
+      desa_id,
+      name,
+      address,
     });
-    res.json(kelompok);
+    res.status(200).json({ message: 'Kelompok berhasil diperbarui' });
   } catch (error) {
     res.status(500).json({ message: 'INTERNAL SERVER ERROR', error });
   }
