95602f916622fab15f0927d21596576ca6bc1194
Dwika Ilham Zakaria <dwikailham37@gmail.com>
2025-08-15 07:20:35 +0700

    fix(db-config): delete utc

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,9 +4,6 @@ const db = new Sequelize('ppg', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
   timezone: '+07:00', // WIB,
-  dialectOptions: {
-    useUTC: false, // Jangan pakai UTC
-  },
 });
 
 export default db;
